443e6a8928db50f105af9ec30a6ada1734661664
chicori3 <chicori3@naver.com>
2020-11-26 19:45:26 +0900

    First Block

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,26 @@
-interface Human {
-  name: string;
-  age: number;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-const human = {
-  name: "white",
-  age: 66,
-};
+const genesisBlock: Block = new Block(0, "22323232", "", "Hello", 1234);
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age} `;
-};
+let blockchain: [Block] = [genesisBlock]; // TS는 블록만 블록체인에 추가하게 된다
 
-console.log(sayHi(human));
-
-export {};
+console.log(blockchain);

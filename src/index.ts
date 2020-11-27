57993eee6b95d62b96f460425241b61d72757e69
chicori3 <chicori3@naver.com>
2020-11-27 21:25:37 +0900

    First Block 2

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,22 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestamp: number;
+
+  // 블럭을 생성하지 않아도 사용할 수 있는 메서드 생성
+  // static 메서드로 만들어야 사용할 수 있다
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -19,8 +32,14 @@ class Block {
   }
 }
 
+Block.calculateBlockHash(1, "adad", 1, "1ada");
+
 const genesisBlock: Block = new Block(0, "22323232", "", "Hello", 1234);
 
-let blockchain: [Block] = [genesisBlock]; // TS는 블록만 블록체인에 추가하게 된다
+let blockchain: Block[] = [genesisBlock]; // TS는 블록만 블록체인에 추가하게 된다
 
-console.log(blockchain);
+const getBlockChain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
+
+const getNewTimestamp = (): Number => Math.round(new Date().getTime() / 1000);
